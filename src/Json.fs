1cb3f428be1bed86e164d7d03d0861dc3de02254
Corey Thomasson <corey@cthom06.com>
2016-02-22 20:30:47 -0500

    new au

--- src/Json.fs
+++ src/Json.fs
@@ -21,37 +21,36 @@ let private parseStringLit =
         else
             Parser.range
                 ['a';'b';'c';'d';'e';'f';'A';'B';'C';'D';'E';'F';'0';'1';'2';'3';'4';'5';'6';'7';'8';'9']
-                (fun c -> readHex (sprintf "%s%c" h c))
+            >>= (fun c -> readHex (sprintf "%s%c" h c))
     let backSlash =
-        Parser.exactly 'n' (Done '\n')
-        <|> Parser.exactly 'r' (Done '\r')
-        <|> Parser.exactly 'b' (Done '\b')
-        <|> Parser.exactly 'f' (Done '\f')
-        <|> Parser.exactly 't' (Done '\t')
-        <|> Parser.exactly '\\' (Done '\\')
-        <|> Parser.exactly '/' (Done '/')
-        <|> Parser.exactly '"' (Done '"')
-        <|> Parser.exactly 'u' (readHex "")
+        (Parser.exactly 'n' *> (Done '\n'))
+        <|> (Parser.exactly 'r' *> (Done '\r'))
+        <|> (Parser.exactly 'b' *> (Done '\b'))
+        <|> (Parser.exactly 'f' *> (Done '\f'))
+        <|> (Parser.exactly 't' *> (Done '\t'))
+        <|> (Parser.exactly '\\' *> (Done '\\'))
+        <|> (Parser.exactly '/' *> (Done '/'))
+        <|> (Parser.exactly '"' *> (Done '"'))
+        <|> (Parser.exactly 'u' *> (readHex ""))
     let rec readChars l =
-        Parser.exactly '\\' backSlash
-        <|> Parser.except ['\\'; '"'] Done
-    Parser.exactly '"' (Done ())
-    *> Parser.repeat (readChars [])
-    >>= (Done >> Parser.exactly '"')
+        (Parser.exactly '\\' *> backSlash)
+        <|> Parser.except ['\\'; '"']
+    Parser.exactly '"'
+    *> Parser.through (Parser.repeat (readChars [])) (Parser.exactly '"')
     >>= (Array.ofSeq >> System.String >> Done)
 
 let private parseNum =
     // BUG: doesn't care about leading zeros
     let range = ['0';'1';'2';'3';'4';'5';'6';'7';'8';'9']
-    let digit = Parser.range range Done
+    let digit = Parser.range range
     let digits = digit >>= (fun c -> Parser.repeat digit <*> cons c)
-    let eSign = Parser.range ['+'; '-'] (fun c -> Done [c]) <|> Done []
+    let eSign = Parser.range ['+'; '-'] >>= (fun c -> Done [c]) <|> Done []
     let e =
-        Parser.range ['e'; 'E'] (fun c ->
+        Parser.range ['e'; 'E'] >>= (fun c ->
             eSign >>= (fun sgn ->
                 digits <*> (fun num -> [c]@sgn@num)))
-    let dec = digits >>= (fun d -> Parser.exactly '.' (digits <*> (fun d2 -> d@('.'::d2))))
-    let minus = Parser.exactly '-' (Done ['-'])
+    let dec = digits >>= (fun d -> Parser.exactly '.' *> (digits <*> (fun d2 -> d@('.'::d2))))
+    let minus = Parser.exactly '-' *> (Done ['-'])
     (minus <|> Done []) >>= (fun sgn ->
         (dec <|> digits) >>= (fun bse ->
             printfn "read a base %A" bse
@@ -60,12 +59,12 @@ let private parseNum =
     <*> (Array.ofSeq >> System.String >> System.Double.Parse) <*> Number
 
 let private parseString = parseStringLit <*> String
-let private parseTrue = Parser.sequence "true" (Bool true)
-let private parseFalse = Parser.sequence "false" (Bool false)
+let private parseTrue = Parser.sequence "true" *>  Done (Bool true)
+let private parseFalse = Parser.sequence "false" *> Done (Bool false)
 let private parseBool = parseTrue <|> parseFalse
-let private parseNull = Parser.sequence "null" Null
+let private parseNull = Parser.sequence "null" *> Done Null
 
-let private wsc c = Parser.eatWhite *> Parser.exactly c (Done ())
+let private wsc c = Parser.eatWhite *> Parser.exactly c
 
 let private csv p =
     p >>= (fun v -> Parser.repeat (wsc ',' *> p) <*> cons v)
@@ -81,7 +80,7 @@ let rec private parseValue () =
 and private parseArray () =
     let oneValue = Parser.eatWhite >>= parseValue
     let endBracket l = wsc ']' *> Done l
-    Parser.exactly '[' (Done ())
+    Parser.exactly '['
     *> (endBracket [] <|> (csv oneValue >>= endBracket))
     <*> List
 
@@ -90,7 +89,7 @@ and private parseObject () =
     let value = Parser.eatWhite >>= parseValue
     let pair = key >>= (fun k -> wsc ':' *> value <*> (fun v -> k, v))
     let endCurl l = wsc '}' *> Done l
-    Parser.exactly '{' (Done ())
+    Parser.exactly '{'
     *> (endCurl [] <|> (csv pair >>= endCurl))
     <*> (Map.ofSeq >> Object)
 
